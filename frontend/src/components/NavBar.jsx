e1c126392e206daad232dd3dd7bb468ca6030e7a
gappaneo <gabrielneo.2021@scis.smu.edu.sg>
2025-05-02 12:27:01 +0800

    navbar done - minor changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,12 +2,15 @@ import "boxicons";
 
 function NavBar() {
   return (
-    <div className="p-4">
+    <div className="p-6">
       <div className="flex justify-between items-center">
         <div className="text-2xl text-zinc-200 cursor-pointer flex items-center">
           M<box-icon name="book-reader" color="white"></box-icon>dLog
         </div>
-        <button className="flex items-center bg-zinc-800 text-zinc-200 ring-1 ring-zinc-600 hover:ring-zinc-500 px-4 py-2 rounded-full text-sm font-medium">
+        <button
+          className="flex items-center bg-zinc-800 text-zinc-200 ring-1 ring-zinc-600 hover:ring-zinc-500
+         px-4 py-2 rounded-full text-sm font-medium"
+        >
           Menu
           <box-icon name="chevron-down" color="grey"></box-icon>
         </button>
