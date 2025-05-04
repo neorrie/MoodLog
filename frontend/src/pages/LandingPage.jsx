ec0f31cdc06e484c4bac7649d7990980abd369e8
Gabriel <gappaneo@gmail.com>
2025-05-04 16:22:47 +0800

    minor UI margin changes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,15 +5,18 @@ function LandingPage() {
   return (
     <div>
       <NavBar></NavBar>
-      <div className="h-[85dvh] flex flex-col items-center justify-center mx-10 lg:flex-row lg:mx-20">
+      <div
+        className="h-[85dvh] flex flex-col items-center justify-center mx-10
+      lg:flex-row lg:mx-28 xl:mx-46"
+      >
         <div className="max-w-2xl my-6 lg:order-3">
           <img src={landingImg} />
         </div>
         <div className="flex flex-col items-center lg:order-1 lg:items-start">
           <div>
             <p className="text-zinc-300 text-center my-4 text-3xl font-bold md:text-4xl lg:text-left lg:mb-12">
-              Reflect <p className="inline text-emerald-600">Simply,</p> Grow{" "}
-              <p className="inline text-emerald-600">Mindfully</p>
+              reflect <p className="inline text-indigo-500">simply,</p> grow{" "}
+              <p className="inline text-indigo-500">mindfully</p>
             </p>
             <p
               className="text-zinc-300 text-center my-4 md:text-lg 
@@ -21,11 +24,11 @@ function LandingPage() {
             >
               {/* A clean, intuitive journaling app designed to help you pause,
               reflect, and track your thoughts — anytime, anywhere. */}
-              A clean, intuitive journaling app designed to help you pause,
+              a clean, intuitive journaling app designed to help you pause,
               reflect, and track your thoughts — anytime, anywhere. Whether
               you're capturing a fleeting idea, processing your day, or setting
               mindful intentions, our seamless experience supports your mental
-              clarity and personal growth.
+              clarity and personal growth
             </p>
           </div>
           <div className="flex space-x-4 mt-6 lg:space-x-10">
