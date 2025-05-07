6c64f16bc99da536f6e10fbb9e7899507272c37f
gappaneo <gabrielneo.2021@scis.smu.edu.sg>
2025-05-07 17:42:50 +0800

    basic about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,16 +1,35 @@
-function AboutPage(){
-    return (
-        <div>
-        <NavBar></NavBar>
-        <div>
-          <div>
-            <Lottie animationData={doggyAnimation} loop={true} />
-          </div>
-          <p>the creation of this app was inspired by my dog. i thought that
-             it would be nice if i could have a space to store our moments together with/of her, so that when she passes, i will have a repositary of memories that i can look back on. this is also my first project for me to practice my SWE skills</p>
-        </div>
+import Lottie from "lottie-react";
+import doggyAnimation from "../assets/lotties/doggy.json";
+import yumiPlaceholder from "../assets/images/profilePic.png";
+
+function AboutPage() {
+  return (
+    <div>
+      <div>
+        <img src={yumiPlaceholder} />
+        <p>
+          this app was inspired by my dog, yumi. our lives are fleeting,
+          especially for our dogs whose lives are much shorter than ours. as
+          such, i thought that it would be nice if i could start journalling our
+          moments together, so that when she passes on, i would have a
+          repositary containing our memories together and of her
+        </p>
       </div>
-    )
+
+      <div>
+        <div className="w-56 md:w-[100%] md:max-w-108">
+          <Lottie animationData={doggyAnimation} loop={true} />
+        </div>
+        <p>
+          from a practical point of view, there are already many existing apps
+          out there that allow users to journal, like notion. however, my other
+          objective was for this project to serve as a way for me to put my SWE
+          skills into practice, and build a personal portfolio that i can show
+          to future employers
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
