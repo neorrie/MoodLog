aafac07f6dfea735280aae795735657cf423a113
gappaneo <gabrielneo.2021@scis.smu.edu.sg>
2025-05-07 17:17:18 +0800

    starting about page

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,14 +1,16 @@
-import NavBar from "../components/NavBar";
-
-function AboutPage() {
-  return (
-    <div>
-      <NavBar></NavBar>
-      <h1>this is the about page</h1>
-      <p>put information here</p>
-      <button>click me</button>
-    </div>
-  );
+function AboutPage(){
+    return (
+        <div>
+        <NavBar></NavBar>
+        <div>
+          <div>
+            <Lottie animationData={doggyAnimation} loop={true} />
+          </div>
+          <p>the creation of this app was inspired by my dog. i thought that
+             it would be nice if i could have a space to store our moments together with/of her, so that when she passes, i will have a repositary of memories that i can look back on. this is also my first project for me to practice my SWE skills</p>
+        </div>
+      </div>
+    )
 }
 
-export default AboutPage;
+export default AboutPage
