28bc553da202383a4afdfff3fedbbad0bedc3d1f
Gabriel <gappaneo@gmail.com>
2025-05-08 12:14:34 +0800

    AboutPage done

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,32 +1,45 @@
 import Lottie from "lottie-react";
 import doggyAnimation from "../assets/lotties/doggy.json";
-import yumiPlaceholder from "../assets/images/profilePic.png";
+import profilePicPlaceholder from "../assets/images/profilePic.png";
+import NavBar from "../components/NavBar";
 
 function AboutPage() {
   return (
-    <div>
-      <div>
-        <img src={yumiPlaceholder} />
-        <p>
-          this app was inspired by my dog, yumi. our lives are fleeting,
-          especially for our dogs whose lives are much shorter than ours. as
-          such, i thought that it would be nice if i could start journalling our
-          moments together, so that when she passes on, i would have a
-          repositary containing our memories together and of her
-        </p>
-      </div>
+    <div className="text-zinc-200 ">
+      <NavBar></NavBar>
 
-      <div>
-        <div className="w-56 md:w-[100%] md:max-w-108">
-          <Lottie animationData={doggyAnimation} loop={true} />
-        </div>
-        <p>
-          from a practical point of view, there are already many existing apps
-          out there that allow users to journal, like notion. however, my other
-          objective was for this project to serve as a way for me to put my SWE
-          skills into practice, and build a personal portfolio that i can show
-          to future employers
+      <div className="flex flex-col p-10 gap-30 md:gap-25 lg:gap-10">
+        <p className="font-bold text-4xl md:text-5xl lg:text-6xl">
+          ABOUT MY MOTIVATION
         </p>
+        <div className="flex flex-col md:flex-row md:gap-20 md:mx-30 items-center justify-center ">
+          <div className="w-76 md:w-1/2">
+            <Lottie animationData={doggyAnimation} loop={true} />
+          </div>
+          <p className="md:w-1/2">
+            This app was born out of love for my dog, Yumi. Our time with our
+            pets is heartbreakingly brief, and I wanted a way to preserve the
+            fleeting moments we share. So, I created a space where I could
+            journal our memories—her quirks, our adventures, and the little
+            things I’ll one day miss. When she’s no longer by my side, this app
+            will be a digital keepsake of her life and our journey together.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center md:gap-20 md:flex-row md:mx-30">
+          <img
+            src={profilePicPlaceholder}
+            className="w-56 md:w-1/2 md:order-2 md:max-w-108"
+          />
+          <p className="my-10 md:order-1 md:w-1/2">
+            While platforms like Notion already offer journalling features, I
+            wanted to build something more personal—and hands-on. This project
+            isn't just a tribute to Yumi; it's also a sandbox for me to sharpen
+            my software engineering skills and craft a meaningful addition to my
+            portfolio. It’s a blend of passion and practice, with a purpose
+            beyond the technical.
+          </p>
+        </div>
       </div>
     </div>
   );
