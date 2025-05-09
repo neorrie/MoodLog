acbe2110a8e6bd88d60b63d6f5ee18a41fd96218
Gabriel <gappaneo@gmail.com>
2025-05-09 12:07:31 +0800

    basic dashboard styling done - static

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,46 @@
 import DashboardNavBar from "../components/DashboardNavBar";
+import "boxicons";
 
 function Dashboard() {
   return (
     <div className="text-zinc-200">
       <DashboardNavBar></DashboardNavBar>
+      <div className="p-4">
+        <div className="flex justify-between items-center">
+          <p className="text-3xl font-bold">Entries</p>
+          <button
+            className="bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-2xl font-semibold cursor-pointer
+          transition ease-in-out duration-200"
+          >
+            + New Entry
+          </button>
+        </div>
+        <div className="bg-zinc-800 mt-4 mb-8 px-4 py-2 w-full rounded-2xl grid grid-cols-[7%_93%]">
+          <box-icon name="search-alt-2" color="grey"></box-icon>
+          <input
+            type="text"
+            className="w-full outline-0"
+            placeholder="Search"
+          />
+        </div>
+        {/* journal entries go in this <div> */}
+        <div
+          className="bg-zinc-800 hover:bg-zinc-900 flex justify-between items-center px-4 py-2 my-4 rounded-2xl
+        cursor-pointer transition ease-in-out duration-200"
+        >
+          <div className="grid grid-cols-[30%_70%] mr-2 ">
+            <p className="col-span-2 font-semibold">Exploring Nature Trails</p>
+            <p className="text-zinc-400">2025-05-05</p>
+            <p className="truncate text-zinc-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              aperiam sed voluptas corporis ratione deleniti esse praesentium
+              tenetur, quidem vero odio repellendus dolor? Impedit similique
+              doloribus consequuntur ducimus eveniet voluptate?
+            </p>
+          </div>
+          <img src="https://placehold.co/55x55" className="rounded-2xl" />
+        </div>
+      </div>
     </div>
   );
 }
