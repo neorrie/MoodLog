9be8aa28947515180c4b22c54679219381443d9b
gappaneo <gabrielneo.2021@scis.smu.edu.sg>
2025-05-12 19:14:52 +0800

    working on popup

--- frontend/src/components/EntryPopup.jsx
+++ frontend/src/components/EntryPopup.jsx
@@ -1,11 +1,12 @@
 function EntryPopup(props) {
   return props.trigger ? (
-    <div className="text-red-500 bg-amber-400">
-      <p>{props.title}</p>
-      <p>{props.date}</p>
-      <p>{props.caption}</p>
-      <img src={props.imgURL} />
-      <button>close</button>
+    <div className="flex flex-col items-center text-zinc-200 bg-zinc-900 p-6 rounded-2xl ring-1 ring-zinc-800">
+      <img src={props.imgURL} className="" />
+      <div>
+        <p>{props.title}</p>
+        <p>{props.date}</p>
+        <p>{props.caption}</p>
+      </div>
     </div>
   ) : (
     ""
