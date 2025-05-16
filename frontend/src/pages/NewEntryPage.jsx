87a683bd9f9e568180b103bfcc1cb1cc2e0a7b72
Gabriel <gappaneo@gmail.com>
2025-05-16 16:19:14 +0800

    image displays now, but resolution still not nice zz

--- frontend/src/pages/NewEntryPage.jsx
+++ frontend/src/pages/NewEntryPage.jsx
@@ -1,7 +1,15 @@
 import DashboardNavBar from "../components/DashboardNavBar.jsx";
 import imgPlaceholder from "../assets/images/cloud-computing.png";
+import { useState, useRef } from "react";
 
 function NewEntryPage() {
+  const [coverImage, setCoverImage] = useState(imgPlaceholder);
+  const imgRef = useRef();
+  const uploadImg = () => {
+    const cachedURL = URL.createObjectURL(imgRef.current.files[0]);
+    setCoverImage(cachedURL);
+  };
+
   return (
     <div>
       <DashboardNavBar></DashboardNavBar>
@@ -17,21 +25,25 @@ function NewEntryPage() {
                 type="file"
                 name="img"
                 id="img"
+                ref={imgRef}
                 accept="image/*"
                 className="hidden"
+                onChange={uploadImg}
               />
               <div
                 className="h-full border-2 border-dotted rounded-2xl border-zinc-400 p-4
-              flex flex-col justify-center items-center gap-4"
+              flex flex-col justify-center items-center gap-4 cursor-pointer"
               >
-                <img src={imgPlaceholder} className="max-w-30" />
-                <p className="opacity-40">
-                  drag and drop or click here to upload image
-                </p>
+                <img src={coverImage} className="max-h-72 object-contain" />
+                {coverImage === imgPlaceholder && (
+                  <p className="opacity-40">
+                    drag and drop or click here to upload image
+                  </p>
+                )}
               </div>
             </label>
           </div>
-          <div className="grid grid-cols-2 gap-4 my-4 w-full">
+          <div className="grid grid-cols-2 gap-4 my-4 w-full flex-grow">
             <div>
               <label htmlFor="title" className="block mb-1">
                 Title
@@ -63,7 +75,7 @@ function NewEntryPage() {
                 name="caption"
                 className="bg-zinc-700 w-full rounded-sm px-2 py-2 focus:outline-1 outline-zinc-500
                 resize-none"
-                rows="4"
+                rows="6"
               ></textarea>
             </div>
             <button className="bg-indigo-500 col-span-2 py-2 rounded-2xl">
